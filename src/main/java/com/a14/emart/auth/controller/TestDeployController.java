84eb2874470034b7c00a22b6d379d7f9b4913075
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-04-17 21:04:18 +0700

    Added controller.

---
+++ src/main/java/com/a14/emart/auth/controller/TestDeployController.java
@@ -0,0 +1,12 @@
+package com.a14.emart.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestDeployController{
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
