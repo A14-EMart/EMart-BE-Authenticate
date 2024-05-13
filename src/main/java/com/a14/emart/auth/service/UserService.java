ba67e42966927940551ac5d020dd6b2c9a42e8be
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-05-13 21:48:30 +0700

    Fixed user service

--- src/main/java/com/a14/emart/auth/service/UserService.java
+++ src/main/java/com/a14/emart/auth/service/UserService.java
@@ -3,8 +3,10 @@ package com.a14.emart.auth.service;
 import com.a14.emart.auth.model.User;
 import com.a14.emart.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
     UserRepository userRepository;
     public User findByUsernameOrEmail(String username, String email) {
